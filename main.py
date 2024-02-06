aab1a2641c6c29d748322323485f3e7d83f9e5ea
ColtonBarnacle <157512471+ColtonBarnacle@users.noreply.github.com>
2024-02-06 09:35:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-#REMOVE PASS AND FIX THIS FUNCTION
-def palindrome(word):
-    pass
+def palindrome(word_one):
+    if word_one.isspace():
+        return 'False'
+    elif word_one[0]==word_one[-1] and word_one[1]==word_one[-2] and word_one[2]==word_one[-3]:
+        return 'True '
+    else:
+        return 'False'
+        
 
 #YOUR CODE GOES HERE
+word_one = input()
+word_one = word_one.replace(" ", "")
+
+print(palindrome(word_one))
