4c8e5c7e658355036f4568c359fb5392744bc183
ColtonBarnacle <colton.barnacle@students.fhu.edu>
2024-02-08 10:16:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-def is_palindrome(text):
-    # Removing white spaces and converting to lowercase
+def palindrome(text):
     text = text.replace(" ", "").lower()
     
-    # Checking if the text is empty
     if not text:
         return False
     
-    # Checking if the text is a palindrome
     return text == text[::-1]
 
 def main():
-    # Getting input from the user
     user_input = input("Enter a text: ")
     
-    # Checking if the input is a palindrome
-    if is_palindrome(user_input):
+    if palindrome(user_input):
         print('True')
     else:
         print('False')
